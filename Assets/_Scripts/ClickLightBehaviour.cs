fffa567d598347b976f47e9abed47414197f3557
Ryan Brooks <46729091+Ryan64Brooks@users.noreply.github.com>
2021-09-24 19:10:53 -0600

    Created Basic Sonar Animation
    Created Sonar Anim

--- Assets/_Scripts/ClickLightBehaviour.cs
+++ Assets/_Scripts/ClickLightBehaviour.cs
@@ -17,7 +17,7 @@ public class ClickLightBehaviour : MonoBehaviour
         //update circle to mouse click !!! eventually trigger this with sound at sound location
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            
+            //TODO only place a circle if this position is (close to being) on screen!
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos.z = 0;
             Instantiate(circle, pos, Quaternion.identity);
--- Assets/_Scripts/FogCircle.cs
+++ Assets/_Scripts/FogCircle.cs
@@ -8,10 +8,12 @@ public class FogCircle : MonoBehaviour
     public float decayRate = 0.1f;
     public GameObject blue;
     public float volume = 0.5f;
+    SpriteRenderer ring_r;
     SpriteRenderer blueR;
     // Start is called before the first frame update
     void Start()
     {
+        ring_r = this.gameObject.GetComponent<SpriteRenderer>();
         this.gameObject.transform.localScale = new Vector3(volume, volume, volume);
         blueR = blue.GetComponent<SpriteRenderer>();
     }
@@ -19,12 +21,12 @@ public class FogCircle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (blueR.color.a <= 0)
+        if (blueR.color.a <= 0 || !ring_r.isVisible)
         {
             Object.Destroy(this.gameObject);
         }
         blueR.color = new Color(0, 0, 1, blueR.color.a - (decayRate * Time.deltaTime));
+        ring_r.color = new Color(1, 1, 1, ring_r.color.a - (decayRate * Time.deltaTime));
     }
 
     private void OnTriggerStay2D(Collider2D col)
