ef6f2580a3640e268439a5b7108fe81a02b18a13
KatrinaM6 <77701413+KatrinaM6@users.noreply.github.com>
2021-10-03 23:00:08 -0600

    Started Movement
    The movement is super janky and bad but I'm tired and not getting anywhere :)

--- Assets/_Scripts/LilyScript.cs
+++ Assets/_Scripts/LilyScript.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class LilyScript : MonoBehaviour
 {
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
+
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+
     }
 }
+
