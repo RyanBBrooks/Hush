32fc6e2953a512a673116e9e8655096d0db1b096
Ryan Brooks <46729091+Ryan64Brooks@users.noreply.github.com>
2021-10-04 18:21:58 -0600

    Fixed several major bugs, changed ground check from raycast to box to account for ledges

--- Assets/_Scripts/LilyScript.cs
+++ Assets/_Scripts/LilyScript.cs
@@ -10,10 +10,10 @@ public class LilyScript : MonoBehaviour
     public float HAcc = 5f;
     public float HDecc = 40f;
     public float JumpImpulse = 100f;
-    public float AirRes = 5;
     Vector3 velocity = new Vector3(0, 0, 0);
     float acc = 0;
     float dToGrnd = 0;
+    bool onGround = false;
     Rigidbody2D rb;
     Collider2D coll;
    
@@ -27,15 +27,23 @@ public class LilyScript : MonoBehaviour
         dToGrnd = coll.bounds.extents.y;
     }
 
-    bool IsOnGround()
+    private void OnTriggerExit2D(Collider2D col)
     {
-        Debug.DrawRay(transform.position, Vector2.down, Color.black);
-        RaycastHit2D hit = 
-            Physics2D.Raycast(transform.position, Vector2.down, dToGrnd + 0.01f, ground);
-        return hit.collider != null;
-       
+        GameObject o = col.gameObject;
+        if (o.layer == 7)
+        {
+            onGround = false;
+        }
     }
-    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject o = col.gameObject;
+        if (o.layer == 7)
+        {
+            onGround = true;
+        }
+    }
+
 
     void Update()
     {
@@ -43,8 +51,8 @@ public class LilyScript : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         //if player not horizontally moving
         //horizontal movement code
-        bool grounded = IsOnGround();
-        Debug.Log(grounded + "  " + dToGrnd + "  " + x);
+
+        //Debug.Log(onGround + "  " + dToGrnd + "  " + x);
 
         if (Mathf.Abs(x) > 0.1) { 
             velocity.x = x * maxHVel;
@@ -59,24 +67,32 @@ public class LilyScript : MonoBehaviour
         velocity.x = Mathf.Lerp(rb.velocity.x, velocity.x, Time.deltaTime * acc);
 
         //jump code
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
-            velocity.y = JumpImpulse + velocity.x;
+            velocity.y = JumpImpulse + Mathf.Abs(velocity.x)/4;
         }
-        else if (!grounded) 
+        else if (!onGround)
         {
-            velocity.y = rb.velocity.y - AirRes;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                velocity.y = rb.velocity.y;
+            }
+            else
+            {
+                velocity.y = rb.velocity.y;
+            }
+            
         }
         else
         {
             velocity.y = rb.velocity.y;
         }
-        if (Input.GetKey("space") && !grounded)
-        {
-        }
+
+
 
         rb.velocity = velocity;
-        
+
+        Debug.Log(rb.velocity.x + "   " + rb.velocity.y);
     }
 }
 
