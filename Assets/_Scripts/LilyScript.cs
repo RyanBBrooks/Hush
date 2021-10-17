30263d1ecf6b45b48a7d4f353903da19918c7e24
Ryan Brooks <46729091+Ryan64Brooks@users.noreply.github.com>
2021-10-16 18:51:57 -0600

    dONT TOUHC THIS I SWEAR TO GOD ITS NOT FINISHED IF YOYU TOUCH THIS YOU WILL BREAK EVERYOTING

--- Assets/_Scripts/LilyScript.cs
+++ Assets/_Scripts/LilyScript.cs
@@ -10,11 +10,13 @@ public class LilyScript : MonoBehaviour
     public float smoothing = 20f;
     public float jumpForce = 5f;
     bool onGround = false;
-    bool jumping = false;
     bool isMoveInput = false; // relates to the pusing / pulling aniations, whether or not the player is trying to move
+    bool isPushingNoGrab = false;
     private Vector3 vel = Vector3.zero;
     Rigidbody2D body;
     SpriteRenderer sprite;
+    GameObject grabTarget;
+    FixedJoint2D joint;
     public Animator anim;
 
 
@@ -25,6 +27,12 @@ public class LilyScript : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
         gameObject.GetComponent<SpriteRenderer>().color = new Color(255,255,255);
         anim = GetComponent<Animator>();
+    }
+
+    public void SetTarget(GameObject t)
+    {
+        Debug.Log(t);
+        grabTarget = t;
     }
 
     //Determine if lily is on the ground
@@ -46,6 +54,11 @@ public class LilyScript : MonoBehaviour
         }
     }
 
+    private bool isGrabbing()
+    {
+        return joint != null;
+    }
+
     //As long as lily is touching an object it will not fade, position will update
     private void OnCollisionExit2D(Collision2D col)
     {
@@ -57,9 +70,11 @@ public class LilyScript : MonoBehaviour
 
             //stop possible pushing animation
             anim.SetBool("isPushing", false);
-          //  Debug.Log("STOP");
+            isPushingNoGrab = false;
         }
     }
+
+
     private void OnCollisionStay2D(Collision2D col)
     {
         GameObject o = col.gameObject;
@@ -76,27 +91,26 @@ public class LilyScript : MonoBehaviour
             }
             avgNormal /= col.contactCount; //normalize
             bool inHorizContact = Mathf.Abs(avgNormal.y) < 0.2;
-            Debug.Log(inHorizContact);
-            if (isMoveInput && inHorizContact && onGround)
+            //Debug.Log(inHorizContact);
+            if (!isGrabbing())
             {
-                anim.SetBool("isPushing", true);
-                //Debug.Log("PUSH");
-            }
-            else
-            { 
-                anim.SetBool("isPushing", false);
-               // Debug.Log("STOP");
+                if (inHorizContact && onGround && isMoveInput)
+                {
+                    anim.SetBool("isPushing", true);
+                    isPushingNoGrab = true;
+                }
+                else
+                {
+                    isPushingNoGrab = false;
+                    anim.SetBool("isPushing", false);
+                    // Debug.Log("STOP");
+                }
             }
         }
     }
 
     void Update()
     {
-        //Stop Jumping
-        if (onGround && jumping)
-        {
-            jumping = false;
-        }
 
         //Horizontal Movement
         float x = Input.GetAxis("Horizontal");
@@ -110,43 +124,66 @@ public class LilyScript : MonoBehaviour
         {
             isMoveInput = true;
         }
+
+        //cut speed in half if we are pulling something
+        float m_speed = max_speed;
+        if (isGrabbing() || isPushingNoGrab)
+        {
+            m_speed /= 2;
+        }
+
         //Update Velocity
-        Vector3 target = new Vector2(x * max_speed, body.velocity.y);
+        Vector3 target = new Vector2(x * m_speed, body.velocity.y);
         body.velocity = Vector3.SmoothDamp(body.velocity, target, ref vel, smoothing);
         //flip sprite
         anim.SetFloat("Speed", Mathf.Abs(x));
-
-        if (x < 0 && body.velocity.x < 0)
+        if (!isGrabbing())
         {
-            sprite.flipX = true;
+            if (x < 0 && body.velocity.x < 0)
+            {
+                sprite.flipX = true;
+                GrabBehavior s = transform.GetChild(1).GetComponent<GrabBehavior>();
+                s.flipX(true);
+            }
+            if (x > 0 && body.velocity.x > 0)
+            {
+                sprite.flipX = false;
+                GrabBehavior s = transform.GetChild(1).GetComponent<GrabBehavior>();
+                s.flipX(true);
+            }
         }
-        if (x > 0 && body.velocity.x > 0)
-        {
-            sprite.flipX = false;
-            
-
-        }
-
         //Jumping
         if (
             (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0)) &&
             onGround)
         {
             onGround = false;
-            jumping = true;
             body.AddForce(new Vector2(0f, jumpForce));
         }
         //Holding space increases float
         else if (
             !(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Joystick1Button0)) &&
-            !onGround &&
-            jumping)
+            !onGround)
         {
-            body.AddForce(new Vector2(0f, -1f));
+            body.AddForce(new Vector2(0f, -3f));
         }
         body.AddForce(new Vector2(0f, -1f));
 
-        //Debug.Log(body.velocity.x + "   " + body.velocity.y + "   " + onGround + "  " + jumping);
+        //start Grabbing
+        if (Input.GetKey(KeyCode.LeftShift) && grabTarget!=null && !isGrabbing())
+        {
+            joint = this.gameObject.AddComponent<FixedJoint2D>();
+            joint.connectedBody = grabTarget.GetComponent<Rigidbody2D>();
+            isPushingNoGrab = false;
+        }
+        //stop grabbing
+        if (isGrabbing() && (!onGround || !Input.GetKey(KeyCode.LeftShift) || grabTarget == null))
+        {
+            Destroy(joint);
+        }
+
+        //Debug.Log(isGrabbing() + "    " + !onGround + "   " + grabTarget);
+        
     }
 }
 
