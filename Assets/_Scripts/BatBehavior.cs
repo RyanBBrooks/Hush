7241f5425d1f8a7bde8767a074c6e1bfbbc82f60
Ryan Brooks <46729091+Ryan64Brooks@users.noreply.github.com>
2021-11-07 14:47:20 -0700

    Put Bat Behavior in Tutorial

--- Assets/_Scripts/BatBehavior.cs
+++ Assets/_Scripts/BatBehavior.cs
@@ -7,6 +7,8 @@ public class BatBehavior : MonoBehaviour
 {
     //movement vars
     public float speed = 2.5f;
+    public float waitTime = 2;
+    float waitTimer = 0;
     
     //screech vars
     public float screechDist = 0.5f; //minimum distance to screech from target
@@ -64,62 +66,73 @@ public class BatBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-        Camera cam = Camera.main;
-        CameraBehavior s = cam.GetComponent<CameraBehavior>();
-        Vector2 newPos, currentPos = this.gameObject.transform.position;
-        Vector2[] posns = calculateStartEnd();
-        Vector2 startPos = posns[0];
-        Vector2 endPos = posns[1];
-
-        //if targets is empty and we have reached end position
-        if (targets.Count <= 0 && isWithinDist(endPos, 0.5f, currentPos))
+        //count down timer
+        if (waitTimer > 0)
         {
-            //reset position to the end
-            newPos = endPos;
+            waitTimer -= Time.deltaTime;
         }
-        //if targets is empty or not near the end position 
-        else
+        //if we arent waiting 
+        else 
         {
-            //Do Movement
 
-            //if we have targets go to those
-            if (targets.Count > 0)
+            Camera cam = Camera.main;
+            CameraBehavior s = cam.GetComponent<CameraBehavior>();
+            Vector2 newPos, currentPos = this.gameObject.transform.position;
+            Vector2[] posns = calculateStartEnd();
+            Vector2 startPos = posns[0];
+            Vector2 endPos = posns[1];
+
+            //if targets is empty and we have reached end position
+            if (targets.Count <= 0 && isWithinDist(endPos, 0.5f, currentPos))
             {
-                newPos = Vector2.Lerp(currentPos, targets[0], Time.deltaTime * speed);
+                //reset position to the end
+                newPos = endPos;
             }
-            //otherwise go to end position
+            //if targets is empty or not near the end position 
             else
             {
-                newPos = Vector2.Lerp(currentPos, endPos, Time.deltaTime * speed);
+                //Do Movement
+
+                //if we have targets go to those
+                if (targets.Count > 0)
+                {
+                    newPos = Vector2.Lerp(currentPos, targets[0], Time.deltaTime * speed);
+                }
+                //otherwise go to end position
+                else
+                {
+                    newPos = Vector2.Lerp(currentPos, endPos, Time.deltaTime * speed);
+                }
             }
-        }      
-        
-        //flip sprite based on movement dir
-        sprite.flipX = (currentPos - newPos).x > 0;
 
-        //update position
-        this.gameObject.transform.position = newPos;
-        Debug.Log(newPos + " " + startPos + " " + endPos);
+            //flip sprite based on movement dir
+            sprite.flipX = (currentPos - newPos).x > 0;
 
-        //If we approximately reach the target, then deal with screeching
-        if (targets.Count>0 && isWithinDist(targets[0], screechDist, this.gameObject.transform.position) && targets.Count > 0)
-        {
-            float volume = defaultVolume;
-            //determine volume
-            if(volumes[0] >= 0)
+            //update position
+            this.gameObject.transform.position = newPos;
+            Debug.Log(newPos + " " + startPos + " " + endPos);
+
+            //If we approximately reach the target, then deal with screeching
+            if (targets.Count > 0 && isWithinDist(targets[0], screechDist, this.gameObject.transform.position) && targets.Count > 0)
             {
-                volume = volumes[0];
+                float volume = defaultVolume;
+                //determine volume
+                if (volumes[0] >= 0)
+                {
+                    volume = volumes[0];
+                }
+                //TODO: play the actual bat audio file here
+
+                //Create a EchoCircle at the sound location
+                s.SpawnEchoCircle(targets[0], volume);
+
+                //start wait timer
+                waitTimer = waitTime;
+
+                //Delete the reached target
+                targets.RemoveAt(0);
+                volumes.RemoveAt(0);
             }
-            //TODO: play the actual bat audio file here
-
-            //Create a EchoCircle at the sound location
-            s.SpawnEchoCircle(targets[0], volume);
-
-            //Delete the reached target
-            targets.RemoveAt(0);
-            volumes.RemoveAt(0);
         }
     }
 }
