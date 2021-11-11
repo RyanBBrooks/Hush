1b11496b4ef88b43469a57116e16dcadc3a72c11
KatrinaM6 <77701413+KatrinaM6@users.noreply.github.com>
2021-11-10 18:20:59 -0700

    added some monster stuff pt.1

---
+++ Assets/_Scripts/MonsterBehavior.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public GameObject lily;
+    int groundLayer = 7; //layer mask referring to the ground layer (layer #7 should be)
+    public bool roar = false;
+    public AudioSource audioSource;
+    public float vol;
+    public float height = 20;
+    Rigidbody2D body;
+    private Vector3 vel = Vector3.zero;
+    public float smoothing = 0.01f; //movement smoothing
+
+    SpriteRenderer sprite;
+    bool flipX = false;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, lily.gameObject.transform.position, step);
+            float x = this.transform.position.x;
+            if (x < 0 && body.velocity.x < 0)
+            {
+                sprite.flipX = flipX = true;
+            }
+            if (x > 0 && body.velocity.x > 0)
+            {
+            sprite.flipX = flipX = false;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject o = collision.gameObject;
+        if (o.tag == "Player")
+        {
+            speed = 0;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        speed = 3;
+    }
+
+    //private void screech()
+    //{
+    //    Debug.Log("sceam");
+    //    Spawn an echo circle
+    //    CameraBehavior s = Camera.main.GetComponent<CameraBehavior>();
+    //    set circle position at foot and slightly in front of character to account for movement speeed
+    //    Vector2 location = new Vector2(this.gameObject.transform.position.x + height,
+    //                                        this.gameObject.transform.position.y - this.gameObject.transform.localScale.y); ;
+    //    s.SpawnEchoCircle(location, vol);
+    //    audioSource.Play();
+
+    //}
+
+
+}
