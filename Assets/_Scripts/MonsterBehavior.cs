d9405555673fc4c69de69da69e5d1c480101ebf0
Ryan Brooks <46729091+Ryan64Brooks@users.noreply.github.com>
2021-11-17 17:57:17 -0700

    Revert "added monster anim"
    This reverts commit 9b60c339c9a97397c39c5796e5519f63e2ba4be9.

--- Assets/_Scripts/MonsterBehavior.cs
+++ Assets/_Scripts/MonsterBehavior.cs
@@ -21,28 +21,27 @@ public class MonsterBehavior : MonoBehaviour
     Animator animate;
     void Start()
     {
-       // body = GetComponent<Rigidbody2D>();
+        body = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         animate = GetComponent<Animator>();
-
     }
 
     
     // Update is called once per frame
     void Update()
     {
-        animate.SetBool("playWalk", true);
+        animate.SetFloat("Speed", speed);
         float step = speed * Time.deltaTime; // calculate distance to move
             transform.position = Vector3.MoveTowards(transform.position, lily.gameObject.transform.position, step);
             float x = this.transform.position.x;
-        //    if (x < 0 && body.velocity.x < 0)
-        //    {
-        //        sprite.flipX = flipX = true;
-        //    }
-        //    if (x > 0 && body.velocity.x > 0)
-        //    {
-        //    sprite.flipX = flipX = false;
-        //}
+            if (x < 0 && body.velocity.x < 0)
+            {
+                sprite.flipX = flipX = true;
+            }
+            if (x > 0 && body.velocity.x > 0)
+            {
+            sprite.flipX = flipX = false;
+        }
 
     }
 
